7da7528b74f856c722853a2a4f9e52e266fae7d6
Nagireddy-SaiKavya <saikavya.n123@gamil.com>
2024-03-10 11:55:21 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+        This is an about component
+        <p>lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Doloribus, quos. Quisquam, quae.
+            </p>
+      
+    </div>
+  )
+}
+
